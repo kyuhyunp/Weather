ffecfb0f51c97e09e4ee0d83e83e2412c68b8935
Kyuhyun Park <kyuhyunp@umich.edu>
2024-01-10 14:47:55 -0800

    current weather and upcoming weather components

---
+++ WeatherProject/src/components/CurrentWeather.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import { Feather } from '@expo/vector-icons';
+
+
+
+const CurrentWeather = () => {
+  return (
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Feather name="sun" size={100} color="black" />
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 8</Text>
+          <Text style={styles.highLow}>Low: 6</Text>
+        </View>
+      </View>
+
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.description}>It's sunny</Text>
+        <Text style={styles.message}>It's perfect t-shirt weather</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: 'pink',
+    flex: 1
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  temp: {
+    color: 'black',
+    fontSize: 48
+  },
+  feels: {
+    fontSize: 30,
+    color: 'black'
+  },
+  highLow: {
+    fontSize: 20,
+    color: 'black'
+  },
+  highLowWrapper: {
+    flexDirection: 'row'
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+    paddingLeft: 35,
+    marginBottom: 40
+  },
+  description: {
+    fontSize: 48
+  },
+  message: {
+    fontSize: 30
+  }
+})
+
+export default CurrentWeather
---
+++ WeatherProject/src/components/UpcomingWeather.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { SafeAreaView, Text, StyleSheet } from 'react-native'
+
+const UpcomingWeather = () => {
+    return (
+        <SafeAreaView styles={styles.container}>
+            <Text>Upcoming Weather</Text>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+})
+export default UpcomingWeather;

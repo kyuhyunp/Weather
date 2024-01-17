c55c954a92666b6711234ec8498de825ec4e1529
Kyuhyun Park <kyuhyunp@umich.edu>
2024-01-17 13:24:35 -0800

    adding images

--- WeatherProject/src/screens/UpcomingWeather.js
+++ WeatherProject/src/screens/UpcomingWeather.js
@@ -1,6 +1,14 @@
 import React from 'react'
-import { SafeAreaView, Text, StyleSheet, FlatList, View, StatusBar } from 'react-native'
-import { Feather } from '@expo/vector-icons'
+import { 
+    SafeAreaView, 
+    Text, 
+    StyleSheet, 
+    FlatList, 
+    View, 
+    StatusBar, 
+    ImageBackground 
+} from 'react-native'
+import ListItem from '../components/ListItem'
 
 const DATA = [
     {
@@ -41,35 +49,29 @@ const DATA = [
     }
 ]
 
-const Item = (props) => {
-    const { dt_txt, min, max, condition} = props
-    return (
-        <View style={styles.item}>
-            <Feather name={'sun'} size={50} color={'white'} />
-            <Text style={styles.date}>{dt_txt}</Text>
-            <Text style={styles.temp}>{min}</Text>
-            <Text style={styles.temp}>{max}</Text>
-        </View>
-    )
-}
 const UpcomingWeather = () => {
     const renderItem = ({ item }) => (
-        <Item 
+        <ListItem 
             condition={item.weather[0].main} 
             dt_txt={item.dt_txt} 
             min={item.main.temp_min} 
             max={item.main.temp_max} 
         />
     )
-
+    const { container, image } = styles
     return (
-        <SafeAreaView style={styles.container}>
-            <Text>Upcoming Weather</Text>
-            <FlatList 
-            data={DATA} 
-            renderItem={renderItem} 
-            keyExtractor={(item) => item.dt_txt}
-            />
+        <SafeAreaView style={container}>
+            <ImageBackground
+                source={require('../../assets/upcoming_weather_background.jpg')} 
+                style={image}
+            >
+                <Text>Upcoming Weather</Text>
+                <FlatList 
+                data={DATA} 
+                renderItem={renderItem} 
+                keyExtractor={(item) => item.dt_txt}
+                />
+            </ImageBackground>
         </SafeAreaView>
     )
 }
@@ -78,25 +80,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         marginTop: StatusBar.currentHeight || 0,
-        backgroundColor: 'red'
+        backgroundColor: 'black'
     },
-    item: {
-        padding: 20,
-        marginVertical: 8,
-        marginHorizontal: 16,
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        borderWidth: 5,
-        backgroundColor: 'pink',
-    },
-    temp: {
-        color: 'white',
-        fontSize: 20
-    },
-    date: {
-        color: 'white',
-        fontSize: 15
+    image: {
+        flex: 1
     }
 })
 export default UpcomingWeather;
